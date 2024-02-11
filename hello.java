174b8f2f65a15c7bc83fa7d165c539ef99e28376
jiswanth <bukkejiswanth3512@gmail.com>
2024-02-11 12:36:20 +0530

    files changed

--- hello.java
+++ hello.java
@@ -5,7 +5,7 @@ public class hello{
     public static void display5(String[] args) {
         System.out.println("hello21243563");
     }
-    public static void display6(String[] args) {
+    public static void display7(String[] args) {
         System.out.println("hello21243563");
     }
     
