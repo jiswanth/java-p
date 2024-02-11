8dbcc9c5383ad650437dc230aa45b8bf1fd2c88f
jiswanth <bukkejiswanth3512@gmail.com>
2024-02-11 12:31:00 +0530

    modified files

--- hello.java
+++ hello.java
@@ -2,4 +2,13 @@ public class hello{
     public static void main(String[] args) {
         System.out.println("hello21243563");
     }
+    public static void display5(String[] args) {
+        System.out.println("hello21243563");
+    }
+    public static void display6(String[] args) {
+        System.out.println("hello21243563");
+    }
+    public static void display7(String[] args) {
+        System.out.println("hello21243563");
+    }
 }
